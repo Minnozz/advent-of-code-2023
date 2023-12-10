b73e21069c5dac59bf5778c723069ad71fee7c8b
Bart Schuurmans <bart@minnozz.com>
2023-12-10 10:54:42 +0100

    day2 part2

--- day2/main.py
+++ day2/main.py
@@ -6,21 +6,40 @@ maximums = {
     "blue": 14,
 }
 
-part1 = 0
 line_format = re.compile(r"^Game (\d+): (.+)$")
+
+part1 = 0
+part2 = 0
 
 with open("day2/input.txt", "r") as f:
     for line in f.readlines():
         m = line_format.search(line)
         game_number = int(m.group(1))
         possible = True
+        minimums = {
+            "red": 0,
+            "green": 0,
+            "blue": 0,
+        }
         for draw in m.group(2).split("; "):
             for cubes in draw.split(", "):
-                [n, color] = cubes.split(" ")
-                if int(n) > maximums[color]:
+                [n_str, color] = cubes.split(" ")
+                n = int(n_str)
+
+                # part 1
+                if n > maximums[color]:
                     possible = False
-                    break
+
+                # part 2
+                minimums[color] = max(minimums[color], n)
+
+        # part 1
         if possible:
             part1 += game_number
 
+        # part 2
+        power = minimums["red"] * minimums["green"] * minimums["blue"]
+        part2 += power
+
 print("part 1 sum: {}".format(part1))
+print("part 2 sum: {}".format(part2))
